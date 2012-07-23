2a8fd52e58a568efc9f1e684d1abe316e7b3d1e3
falun <github@magichappyplace.com>
2012-07-22 20:54:09 -0700

    remove JD-core line comments, tweak try/catch/throw compile error in OptionsUI

--- src/ants/core/ui/Ants.java
+++ src/ants/core/ui/Ants.java
@@ -1,43 +1,43 @@
-/*    */ package ants.core.ui;
-/*    */ 
-/*    */ import java.awt.Container;
-/*    */ import javax.swing.JFrame;
-/*    */ 
-/*    */ public class Ants extends JFrame
-/*    */ {
-/*  9 */   private static Ants instance = null;
-/*    */ 
-/*    */   public static Ants get() {
-/* 12 */     if (instance == null) {
-/* 13 */       instance = new Ants();
-/*    */     }
-/* 15 */     return instance;
-/*    */   }
-/*    */ 
-/*    */   public Ants() {
-/* 19 */     super("Ants!");
-/*    */ 
-/* 21 */     setDefaultCloseOperation(3);
-/* 22 */     setContentPane(OptionsUI.get());
-/* 23 */     pack();
-/* 24 */     setResizable(false);
-/* 25 */     setLocationRelativeTo(null);
-/* 26 */     setVisible(true);
-/*    */   }
-/*    */ 
-/*    */   public void setContentPane(Container contentPane)
-/*    */   {
-/* 31 */     super.setContentPane(contentPane);
-/* 32 */     invalidate();
-/* 33 */     validate();
-/* 34 */     pack();
-/* 35 */     setLocationRelativeTo(null);
-/*    */   }
-/*    */ 
-/*    */   public static void main(String[] args) throws Exception {
-/* 39 */     get();
-/*    */   }
-/*    */ }
+package ants.core.ui;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+
+public class Ants extends JFrame
+{
+  private static Ants instance = null;
+
+  public static Ants get() {
+    if (instance == null) {
+      instance = new Ants();
+    }
+    return instance;
+  }
+
+  public Ants() {
+    super("Ants!");
+
+    setDefaultCloseOperation(3);
+    setContentPane(OptionsUI.get());
+    pack();
+    setResizable(false);
+    setLocationRelativeTo(null);
+    setVisible(true);
+  }
+
+  public void setContentPane(Container contentPane)
+  {
+    super.setContentPane(contentPane);
+    invalidate();
+    validate();
+    pack();
+    setLocationRelativeTo(null);
+  }
+
+  public static void main(String[] args) throws Exception {
+    get();
+  }
+}
 
 /* Location:           C:\Users\Richard\Projects\Ants\sim-src\
  * Qualified Name:     ants.core.ui.Ants
--- src/ants/core/ui/OptionsUI.java
+++ src/ants/core/ui/OptionsUI.java
@@ -1,203 +1,200 @@
-/*     */ package ants.core.ui;
-/*     */ 
-/*     */ import ants.Ant;
-/*     */ import ants.core.AntsGameModel;
-/*     */ import ants.core.Serialization;
-/*     */ import java.awt.Color;
-/*     */ import java.awt.Font;
-/*     */ import java.awt.datatransfer.DataFlavor;
-/*     */ import java.awt.datatransfer.Transferable;
-/*     */ import java.awt.event.ActionEvent;
-/*     */ import java.awt.event.ActionListener;
-/*     */ import java.io.File;
-/*     */ import java.io.FileNotFoundException;
-/*     */ import java.io.PrintStream;
-/*     */ import java.util.List;
-/*     */ import javax.swing.JButton;
-/*     */ import javax.swing.JFileChooser;
-/*     */ import javax.swing.JLabel;
-/*     */ import javax.swing.JOptionPane;
-/*     */ import javax.swing.JPanel;
-/*     */ import javax.swing.JSpinner;
-/*     */ import javax.swing.JTextField;
-/*     */ import javax.swing.SpinnerNumberModel;
-/*     */ import javax.swing.TransferHandler;
-/*     */ import javax.swing.TransferHandler.TransferSupport;
-/*     */ import javax.swing.filechooser.FileFilter;
-/*     */ import javax.tools.JavaCompiler;
-/*     */ import javax.tools.ToolProvider;
-/*     */ import net.miginfocom.swing.MigLayout;
-/*     */ 
-/*     */ public class OptionsUI extends JPanel
-/*     */ {
-/*     */   private static OptionsUI instance;
-/*     */   private JTextField fileField;
-/*     */   private JSpinner widthSpinner;
-/*     */   private JSpinner heightSpinner;
-/*     */   private JSpinner numStartingAntsSpinner;
-/*     */   private JSpinner numTurnsPerNewAntSpinner;
-/*     */   private JButton browseButton;
-/*     */   private JButton startButton;
-/*     */   private JFileChooser fileChooser;
-/* 183 */   private final ActionListener actionListener = new ActionListener()
-/*     */   {
-/*     */     public void actionPerformed(ActionEvent e) {
-/* 186 */       Object src = e.getSource();
-/* 187 */       if (src == OptionsUI.this.browseButton)
-/* 188 */         OptionsUI.this.showFileChooser();
-/* 189 */       else if (src == OptionsUI.this.startButton)
-/*     */         try
-/*     */         {
-/* 192 */           OptionsUI.this.start();
-/*     */         } catch (Exception ee) {
-/*     */           try {
-/* 195 */             ee.printStackTrace(new PrintStream("ERR.txt"));
-/*     */           }
-/*     */           catch (FileNotFoundException localFileNotFoundException) {
-/*     */           }
-/* 199 */           throw new RuntimeException(ee);
-/*     */         }
-/*     */     }
-/* 183 */   };
-/*     */ 
-/*     */   private OptionsUI()
-/*     */   {
-/*  42 */     initUI();
-/*     */ 
-/*  44 */     initializeListeners();
-/*     */   }
-/*     */ 
-/*     */   public static OptionsUI get() {
-/*  48 */     if (instance == null) {
-/*  49 */       instance = new OptionsUI();
-/*     */     }
-/*  51 */     return instance;
-/*     */   }
-/*     */ 
-/*     */   private void initUI() {
-/*  55 */     setLayout(new MigLayout("insets 10, gap 10"));
-/*     */ 
-/*  57 */     int minSize = 1; int size = 20; int maxSize = 128;
-/*     */ 
-/*  59 */     this.fileField = new JTextField(32);
-/*  60 */     this.browseButton = new JButton("Browse");
-/*  61 */     this.widthSpinner = new JSpinner(new SpinnerNumberModel(size, minSize, maxSize, 1));
-/*  62 */     this.heightSpinner = new JSpinner(new SpinnerNumberModel(size, minSize, maxSize, 1));
-/*  63 */     this.numStartingAntsSpinner = new JSpinner(new SpinnerNumberModel(3, 0, 999, 1));
-/*  64 */     this.numTurnsPerNewAntSpinner = new JSpinner(new SpinnerNumberModel(10, 1, 99, 1));
-/*  65 */     this.startButton = new JButton("Go!");
-/*     */ 
-/*  67 */     this.browseButton.setFocusPainted(false);
-/*  68 */     this.startButton.setFocusPainted(false);
-/*  69 */     this.startButton.setBackground(Color.green);
-/*  70 */     this.startButton.setFont(new Font("Arial", 1, 30));
-/*  71 */     this.fileField.setTransferHandler(new TransferHandler()
-/*     */     {
-/*     */       public boolean canImport(TransferHandler.TransferSupport support)
-/*     */       {
-/*  75 */         return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
-/*     */       }
-/*     */ 
-/*     */       public boolean importData(TransferHandler.TransferSupport support)
-/*     */       {
-/*     */         try
-/*     */         {
-/*  84 */           List files = (List)support.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
-/*  85 */           OptionsUI.this.fileField.setText(((File)files.get(0)).toString());
-/*  86 */           return true; } catch (Exception e) {
-/*     */         }
-/*  88 */         throw new RuntimeException(e);
-/*     */       }
-/*     */     });
-/*  93 */     add(new JLabel("Ant Implementation:"), "");
-/*  94 */     add(this.fileField, "");
-/*  95 */     add(this.browseButton, "wrap");
-/*     */ 
-/* 102 */     add(new JLabel("You can drag and drop your .java or .class file into the implementation field."), "span, wrap");
-/* 103 */     add(this.startButton, "wrap");
-/*     */   }
-/*     */ 
-/*     */   private void showFileChooser()
-/*     */   {
-/* 109 */     if (this.fileChooser == null) {
-/* 110 */       this.fileChooser = new JFileChooser();
-/* 111 */       this.fileChooser.setFileSelectionMode(0);
-/* 112 */       this.fileChooser.setFileFilter(new FileFilter()
-/*     */       {
-/*     */         public boolean accept(File f) {
-/* 115 */           return (f.isDirectory()) || (f.getName().endsWith(".java")) || (f.getName().endsWith(".class"));
-/*     */         }
-/*     */ 
-/*     */         public String getDescription()
-/*     */         {
-/* 120 */           return "JAVA or CLASS file";
-/*     */         }
-/*     */       });
-/*     */     }
-/* 125 */     int i = this.fileChooser.showOpenDialog(this);
-/* 126 */     if (i != 0) {
-/* 127 */       return;
-/*     */     }
-/*     */ 
-/* 130 */     this.fileField.setText(this.fileChooser.getSelectedFile().toString());
-/*     */   }
-/*     */ 
-/*     */   private void start()
-/*     */   {
-/* 135 */     String filePath = this.fileField.getText().toLowerCase();
-/*     */ 
-/* 137 */     if (filePath.isEmpty()) {
-/* 138 */       JOptionPane.showMessageDialog(this, "You must select your Ant implementation!");
-/* 139 */       return;
-/*     */     }
-/* 141 */     if ((!filePath.endsWith(".java")) && (!filePath.endsWith(".class"))) {
-/* 142 */       JOptionPane.showMessageDialog(this, "You must choose a JAVA or CLASS file as your Ant implementation!");
-/* 143 */       return;
-/*     */     }
-/*     */ 
-/* 146 */     if (filePath.endsWith(".java")) {
-/* 147 */       System.out.println("Compiling java file....");
-/* 148 */       JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-/*     */ 
-/* 150 */       if (compiler == null) {
-/* 151 */         JOptionPane.showMessageDialog(null, "No compiler was found -- try using a .class file instead.");
-/* 152 */         return;
-/*     */       }
-/*     */ 
-/* 155 */       int i = compiler.run(null, System.out, System.err, new String[] { filePath });
-/* 156 */       if (i != 0) {
-/* 157 */         JOptionPane.showMessageDialog(this, "Problem Compiling");
-/* 158 */         return;
-/*     */       }
-/*     */ 
-/* 161 */       filePath = filePath.substring(0, filePath.length() - 4) + "class";
-/*     */     }
-/*     */ 
-/* 165 */     File file = new File(this.fileField.getText());
-/* 166 */     if (!file.exists()) {
-/* 167 */       throw new RuntimeException("Could not find the class file -- " + file);
-/*     */     }
-/*     */ 
-/* 170 */     Class c = Serialization.loadClass(file);
-/*     */ 
-/* 172 */     if (!Ant.class.isAssignableFrom(c)) {
-/* 173 */       JOptionPane.showMessageDialog(this, "Your implementation must implement Ant.java!");
-/* 174 */       return;
-/*     */     }
-/*     */ 
-/* 177 */     AntsGameModel model = new AntsGameModel(c, ((Integer)this.widthSpinner.getValue()).intValue(), 
-/* 178 */       ((Integer)this.heightSpinner
-/* 178 */       .getValue()).intValue(), ((Integer)this.numStartingAntsSpinner.getValue()).intValue(), ((Integer)this.numTurnsPerNewAntSpinner.getValue()).intValue());
-/*     */ 
-/* 180 */     Ants.get().setContentPane(new AntsView(model));
-/*     */   }
-/*     */ 
-/*     */   private void initializeListeners()
-/*     */   {
-/* 206 */     this.browseButton.addActionListener(this.actionListener);
-/* 207 */     this.startButton.addActionListener(this.actionListener);
-/*     */   }
-/*     */ }
+package ants.core.ui;
+
+import ants.Ant;
+import ants.core.AntsGameModel;
+import ants.core.Serialization;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
+import javax.swing.filechooser.FileFilter;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import net.miginfocom.swing.MigLayout;
+
+public class OptionsUI extends JPanel
+{
+  private static OptionsUI instance;
+  private JTextField fileField;
+  private JSpinner widthSpinner;
+  private JSpinner heightSpinner;
+  private JSpinner numStartingAntsSpinner;
+  private JSpinner numTurnsPerNewAntSpinner;
+  private JButton browseButton;
+  private JButton startButton;
+  private JFileChooser fileChooser;
+  private final ActionListener actionListener = new ActionListener()
+  {
+    public void actionPerformed(ActionEvent e) {
+      Object src = e.getSource();
+      if (src == OptionsUI.this.browseButton)
+        OptionsUI.this.showFileChooser();
+      else if (src == OptionsUI.this.startButton)
+        try
+        {
+          OptionsUI.this.start();
+        } catch (Exception ee) {
+          try {
+            ee.printStackTrace(new PrintStream("ERR.txt"));
+          }
+          catch (FileNotFoundException localFileNotFoundException) {
+          }
+          throw new RuntimeException(ee);
+        }
+    }
+  };
+
+  private OptionsUI()
+  {
+    initUI();
+
+    initializeListeners();
+  }
+
+  public static OptionsUI get() {
+    if (instance == null) {
+      instance = new OptionsUI();
+    }
+    return instance;
+  }
+
+  private void initUI() {
+    setLayout(new MigLayout("insets 10, gap 10"));
+
+    int minSize = 1; int size = 20; int maxSize = 128;
+
+    this.fileField = new JTextField(32);
+    this.browseButton = new JButton("Browse");
+    this.widthSpinner = new JSpinner(new SpinnerNumberModel(size, minSize, maxSize, 1));
+    this.heightSpinner = new JSpinner(new SpinnerNumberModel(size, minSize, maxSize, 1));
+    this.numStartingAntsSpinner = new JSpinner(new SpinnerNumberModel(3, 0, 999, 1));
+    this.numTurnsPerNewAntSpinner = new JSpinner(new SpinnerNumberModel(10, 1, 99, 1));
+    this.startButton = new JButton("Go!");
+
+    this.browseButton.setFocusPainted(false);
+    this.startButton.setFocusPainted(false);
+    this.startButton.setBackground(Color.green);
+    this.startButton.setFont(new Font("Arial", 1, 30));
+    this.fileField.setTransferHandler(new TransferHandler() {
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+        }
+
+        public boolean importData(TransferHandler.TransferSupport support) {
+            try {
+                List files = (List)support.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+                OptionsUI.this.fileField.setText(((File)files.get(0)).toString());
+                return true;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    });
+    add(new JLabel("Ant Implementation:"), "");
+    add(this.fileField, "");
+    add(this.browseButton, "wrap");
+
+    add(new JLabel("You can drag and drop your .java or .class file into the implementation field."), "span, wrap");
+    add(this.startButton, "wrap");
+  }
+
+  private void showFileChooser()
+  {
+    if (this.fileChooser == null) {
+      this.fileChooser = new JFileChooser();
+      this.fileChooser.setFileSelectionMode(0);
+      this.fileChooser.setFileFilter(new FileFilter()
+      {
+        public boolean accept(File f) {
+          return (f.isDirectory()) || (f.getName().endsWith(".java")) || (f.getName().endsWith(".class"));
+        }
+
+        public String getDescription()
+        {
+          return "JAVA or CLASS file";
+        }
+      });
+    }
+    int i = this.fileChooser.showOpenDialog(this);
+    if (i != 0) {
+      return;
+    }
+
+    this.fileField.setText(this.fileChooser.getSelectedFile().toString());
+  }
+
+  private void start()
+  {
+    String filePath = this.fileField.getText().toLowerCase();
+
+    if (filePath.isEmpty()) {
+      JOptionPane.showMessageDialog(this, "You must select your Ant implementation!");
+      return;
+    }
+    if ((!filePath.endsWith(".java")) && (!filePath.endsWith(".class"))) {
+      JOptionPane.showMessageDialog(this, "You must choose a JAVA or CLASS file as your Ant implementation!");
+      return;
+    }
+
+    if (filePath.endsWith(".java")) {
+      System.out.println("Compiling java file....");
+      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+      if (compiler == null) {
+        JOptionPane.showMessageDialog(null, "No compiler was found -- try using a .class file instead.");
+        return;
+      }
+
+      int i = compiler.run(null, System.out, System.err, new String[] { filePath });
+      if (i != 0) {
+        JOptionPane.showMessageDialog(this, "Problem Compiling");
+        return;
+      }
+
+      filePath = filePath.substring(0, filePath.length() - 4) + "class";
+    }
+
+    File file = new File(this.fileField.getText());
+    if (!file.exists()) {
+      throw new RuntimeException("Could not find the class file -- " + file);
+    }
+
+    Class c = Serialization.loadClass(file);
+
+    if (!Ant.class.isAssignableFrom(c)) {
+      JOptionPane.showMessageDialog(this, "Your implementation must implement Ant.java!");
+      return;
+    }
+
+    AntsGameModel model = new AntsGameModel(c, ((Integer)this.widthSpinner.getValue()).intValue(), 
+      ((Integer)this.heightSpinner
+      .getValue()).intValue(), ((Integer)this.numStartingAntsSpinner.getValue()).intValue(), ((Integer)this.numTurnsPerNewAntSpinner.getValue()).intValue());
+
+    Ants.get().setContentPane(new AntsView(model));
+  }
+
+  private void initializeListeners()
+  {
+    this.browseButton.addActionListener(this.actionListener);
+    this.startButton.addActionListener(this.actionListener);
+  }
+}
 
 /* Location:           C:\Users\Richard\Projects\Ants\sim-src\
  * Qualified Name:     ants.core.ui.OptionsUI
